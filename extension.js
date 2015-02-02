b363ca7382abf66da9684b545e9c06657ecad024
TricKy- <TricKy-@users.noreply.github.com>
2015-02-02 23:08:27 +0000

    Update extension.js

--- extension.js
+++ extension.js
@@ -53,7 +53,7 @@
     //Change the bots default settings and make sure they are loaded on launch
 
     localStorage.setItem("basicBotsettings", JSON.stringify({
-        botName: "basicBot",
+        botName: "GritBot",
         language: "english",
         chatLink: "https://rawgit.com/Yemasthui/basicBot/master/lang/en.json",
         maximumAfk: 120,
@@ -84,7 +84,7 @@
         afkRankCheck: "ambassador",
         motdEnabled: false,
         motdInterval: 5,
-        motd: "Temporary Message of the Day",
+        motd: "Join Our Minecraft Server! play.gritnetwork.me",
         filterChat: true,
         etaRestriction: false,
         welcome: true,
